db6a28bbef473c732d30642b48bbaa82c396612d
Glitch (who-will-do-it) <none>
2020-04-07 13:18:19 +0000

    Show proper user names

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+function arrayToText(a) {
+    if (a.length <= 2) {
+        return a.join(' and ');
+    } else {
+        return a.slice(0, -1).join(', ') + ' and ' + a[a.length-1];
+    }
+}
+
 // setup a new database
 // persisted using async file storage
 // Security note: the database is saved to the file `db.json` on the local filesystem.
@@ -24,15 +32,18 @@ app.command('/who-will-do', async ({ command, ack, say }) => {
   
   // TODO: add, who did it last times
   let text = "Who want's to do the " + chore + " " + when + "? ";
-  var dbUsers=[];
-  var users = db.get(chore).value() // Find all users in the collection
-  if (users) {
-    users.forEach(function(user) {
-      //const user = await app.client.users.profile.get({user: body.user.id});
-      //console.log(user);
-      dbUsers.push([user.name]); // adds their info to the dbUsers value
-    });
-    text = text + dbUsers.join(", ") + " did it recently.";
+  var users=[];
+  var dbUsers = db.get(chore).value() // Find all users in the collection
+  if (dbUsers) {
+    for (let i=dbUsers.length - 1; i >= Math.max(0, dbUsers.length - 3); i--) {
+      const slack_user = await app.client.users.info({user: dbUsers[i].id, token: process.env.SLACK_BOT_TOKEN});
+      users.push(slack_user.user.real_name);
+    }
+    text = text + arrayToText(users) + " did it recently.";
+  } else {
+    const emptyDB = `{ "${chore}": [] }`
+    db.defaults(JSON.parse(emptyDB)).write()
+    console.log(`Database ${chore} created`);
   }
   
   let message_blocks = [
@@ -61,20 +72,17 @@ app.command('/who-will-do', async ({ command, ack, say }) => {
 });
 
 // The middleware will be called every time an interactive component with the action_id “i_will_do" is triggered
-app.action('i_will_do', async({context, action, body, ack, say}) => {
+app.action('i_will_do', async({context, action, body, ack, respond}) => {
+  const actionData = JSON.parse(action.value);
+
   // Acknowledge action request
   ack();
   
-  const actionData = JSON.parse(action.value);
-  
-  // post in the channel, that actionData.user will do the chore
-  const post = await app.client.chat.postMessage({
-    // The token you used to initialize your app is stored in the `context` object
-    token: context.botToken,
-    channel: actionData.channel,
+  respond({
+    replace_original: true,
     text: `<@${body.user.name}> will do the ${actionData.chore} for ${actionData.when}! Please support him as good as you can!`
   });
-  
+ 
   // Thank user
   await app.client.chat.postMessage({
     // The token you used to initialize your app is stored in the `context` object
@@ -89,40 +97,6 @@ app.action('i_will_do', async({context, action, body, ack, say}) => {
     .write();
 });
 
-
-
-// *** Responding a message containing a red circle emoji ***
-app.message(':red_circle:', async ({message, say}) => {
-  console.log(message);
-  const {channel, ts, user} = message;
-  
-  let message_blocks = [
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "Who want's to do the review today?"
-      },
-      "accessory": {
-        "type": "button",
-        "text": {
-          "type": "plain_text",
-          "text": "I will do it!"
-        },
-        "style": "primary",
-        "value": JSON.stringify({ts: ts, channel: channel, user: user}),
-        "action_id": "i_will_do"
-      }
-    }
-  ];
-  
-  // Respond to the message with a button
-  say({
-    blocks: message_blocks
-  });
-});
-
-
 // *** Handle errors ***
 app.error((error) => {
 	// Check the details of the error to handle cases where you should retry sending a message or stop the app
@@ -134,4 +108,9 @@ app.error((error) => {
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
+  console.log(
+    "Your URL for events and interactivity is: https://" +
+      process.env.PROJECT_NAME +
+      ".glitch.me/slack/events"
+  );
 })();
